98c05f3af725b3cf16a1c3088c6f9e3497b9eabd
Toon Spin <toon.spin@gmail.com>
2021-12-02 13:10:12 +0100

    Day 02

---
+++ src/day02.hs
@@ -0,0 +1,31 @@
+data Command = Command Char Int
+data Position = Position Int Int Int
+
+parseCommand :: [String] -> Command
+parseCommand [command, param] = (Command (head command) (read param))
+
+executeCommandP1 :: Position -> Command -> Position
+executeCommandP1 (Position depth distance aim) (Command command param)
+    | command == 'f' = (Position depth (distance + param) aim)
+    | command == 'd' = (Position (depth + param) distance aim)
+    | command == 'u' = (Position (depth - param) distance aim)
+
+executeCommandP2 :: Position -> Command -> Position
+executeCommandP2 (Position depth distance aim) (Command command param)
+    | command == 'f' = (Position (depth + aim * param) (distance + param) aim)
+    | command == 'd' = (Position depth distance (aim + param))
+    | command == 'u' = (Position depth distance (aim - param))
+
+main = do
+    contents <- getContents
+
+    let input = map parseCommand $ map words $ lines contents
+        getOutput = \(Position depth distance _) -> depth * distance
+        output1 = getOutput $ foldl executeCommandP1 (Position 0 0 0) input
+        output2 = getOutput $ foldl executeCommandP2 (Position 0 0 0) input
+
+    putStr "Position times depth for part 1: "
+    putStrLn (show output1)
+
+    putStr "Position times depth for part 2: "
+    putStrLn (show output2)
