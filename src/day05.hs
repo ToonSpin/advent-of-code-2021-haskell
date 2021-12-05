2cf9fc4bb121ba0a2adf29bc27c428346ca06309
Toon Spin <toon.spin@gmail.com>
2021-12-05 12:40:48 +0100

    Day 05

---
+++ src/day05.hs
@@ -0,0 +1,52 @@
+import Data.Char
+import qualified Data.Map as Map
+
+data Point = Point Int Int deriving (Eq, Ord)
+type Line = (Point, Point)
+type OceanFloor = Map.Map Point Int
+
+parsePoint :: String -> Point
+parsePoint s =
+    let s1 = takeWhile isDigit s
+        s2 = drop (length s1 + 1) s
+    in Point (read s1) (read s2)
+
+parseLine :: String -> Line
+parseLine s =
+    let s1 = takeWhile (not . isSpace) s
+        s2 = drop (length s1 + 4) s
+    in (parsePoint s1, parsePoint s2)
+
+isDiagonal :: Line -> Bool
+isDiagonal (Point x1 y1, Point x2 y2) = x1 /= x2 && y1 /= y2
+
+-- This assumes x1  /= x2 || y1 /= y2, because makeRange will return an infinite
+-- list if m == n
+expand :: Line -> [Point]
+expand (Point x1 y1, Point x2 y2) =
+    let makeRange m n = [m, (m + signum (n - m)) .. n]
+        xs = makeRange x1 x2
+        ys = makeRange y1 y2
+    in map (\ (x, y) -> Point x y) $ zip xs ys
+
+markPoint :: Point -> OceanFloor -> OceanFloor
+markPoint p m = Map.insertWith (+) p 1 m
+
+markPoints :: [Point] -> OceanFloor -> OceanFloor
+markPoints ps m = foldr markPoint m ps
+
+countDuplicateElems :: [Line] -> Int
+countDuplicateElems input =
+    let expanded   = map expand $ input
+        oceanFloor = foldr markPoints Map.empty $ expanded
+    in length $ filter (> 1) $ Map.elems $ oceanFloor
+
+main = do
+    contents <- getContents
+    let input = map parseLine $ lines contents
+
+    putStr "Overlapping points among nondiagonal lines: "
+    putStrLn $ show $ countDuplicateElems $ filter (not . isDiagonal) input
+
+    putStr "Overlapping points among all lines: "
+    putStrLn $ show $ countDuplicateElems input
