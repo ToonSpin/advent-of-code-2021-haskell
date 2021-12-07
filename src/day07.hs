9680fa97b947085a97e565db17526d92334eb5a0
Toon Spin <toon.spin@gmail.com>
2021-12-07 20:24:25 +0100

    Day 07

---
+++ src/day07.hs
@@ -0,0 +1,24 @@
+import Data.Char
+
+getInput :: String -> [Int]
+getInput [] = []
+getInput (',':xs) = getInput xs
+getInput xs =
+    let (d, rest) = span isDigit xs
+    in (read d) : getInput rest
+
+fuelCost :: (Int -> Int) -> [Int] -> Int -> Int
+fuelCost f xs n = sum $ map (f . abs . (-) n) xs
+
+partTwo :: Int -> Int
+partTwo x = x * (x + 1) `div` 2
+
+main = do
+    contents <- getContents
+    let input = getInput $ head $ lines contents
+
+    putStr "The least amount of fuel at constant burn rate: "
+    putStrLn $ show $ minimum $ map (fuelCost id input) $ [0..length input]
+
+    putStr "The least amount of fuel at quadratic burn rate: "
+    putStrLn $ show $ minimum $ map (fuelCost partTwo input) $ [0..length input]
